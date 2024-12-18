25a5e4c816acad6027af6bcebfac4e6e7ef99b9b
David Rawnsley <david.rawnsley@jisc.ac.uk>
2024-12-18 11:56:39 +0000

    Update cookie_list.js
    removed unused cookies from necessary list

--- ckanext/ukdstheme/templates/scripts/cookie_list.js
+++ ckanext/ukdstheme/templates/scripts/cookie_list.js
@@ -2,7 +2,7 @@
 const ANALYTICS_COOKIES = [];
 
 // include here any cookies needed for the site to function (e.g. session cookies) that aren't marked hrtpOnly 
-const NECESSARY_COOKIES = ['_pk_ref','_pk_id','_pk_ses','AWSALBCORS','CookieControl','CFID','CFTOKEN','METHOD'];
+const NECESSARY_COOKIES = ['_pk_ref','_pk_id','_pk_ses','AWSALBCORS','CookieControl'];
 
 
 
